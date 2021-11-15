a08df304af4f24f08b28a220fcf96ffee563f437
Erik <erik@vsyke.com>
2021-11-14 18:24:25 -0800

    added typescript

--- dist/index.js
+++ dist/index.js
@@ -1,58 +1,52 @@
+"use strict";
 // #region Supporting Linear Alg
-
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.project4 = exports.project3 = exports.rotateXY4 = exports.rotateXZ4 = exports.rotateXW4 = exports.rotateYZ4 = exports.rotateYW4 = exports.rotateZW4 = exports.rotateZ4 = exports.rotateX4 = exports.rotateZ3 = exports.rotateY3 = exports.rotateX3 = exports.matMul = exports.acceleration = exports.eulerStep = exports.fractionalLine = exports.LerpSmooth1D = void 0;
 //https://www.desmos.com/calculator/c2wkore2bi
 function LerpSmooth1D(x1, x2, p, rate, halfSmooth, infinite) {
-    if(infinite || p <= Math.PI * (halfSmooth ? .5 : 1)) {
+    if (infinite || p <= Math.PI * (halfSmooth ? .5 : 1)) {
         p += rate;
-        return [((Math.sin(p + (1.5 * Math.PI))+1)*((x2-x1)/(halfSmooth ? 1 : 2)) + x1), p];
+        return [((Math.sin(p + (1.5 * Math.PI)) + 1) * ((x2 - x1) / (halfSmooth ? 1 : 2)) + x1), p];
     }
-    return [x2, p]
+    return [x2, p];
 }
-
+exports.LerpSmooth1D = LerpSmooth1D;
 //fraction > 0; fraction = N
 //0 <= fpos < fraction; fpos = N
 //https://www.desmos.com/calculator/tbxtssvtcn
-function fractionalLine(x1,y1,x2,y2,f,p) {
-    if(f <= 0 || p < 0 || p >= f)
-        return [x1,y1,x2,y2];
-    let dx = (x2 - x1)/f;
-    let dy = (y2 - y1)/f;
-    return [dx*p+x1,dy*p+y1,dx*(p+1)+x1,dy*(p+1)+y1];
+function fractionalLine(p1, p2, f, p) {
+    if (f <= 0 || p < 0 || p >= f)
+        return [p1.x, p1.y, p2.x, p2.y];
+    var dx = (p2.x - p1.x) / f;
+    var dy = (p2.y - p1.y) / f;
+    return [dx * p + p1.x, dy * p + p1.y, dx * (p + 1) + p1.x, dy * (p + 1) + p1.y];
 }
-
+exports.fractionalLine = fractionalLine;
 // #region Oribital Mechanics
-
 //https://www.desmos.com/calculator/vbsetpue7m
-function eulerStep(pos1,pos2,vel1,vel2,G) {
-    for(let i = 1; i <= 8; i++) {
-        let dt = 1 / i;
-        let acc1 = acceleration(pos2, pos1,G);
-        let acc2 = acceleration(pos1,pos2,G);
-        vel1 = { x: vel1.x + acc1.x * dt, y: vel1.y + acc1.y * dt };
-        vel2 = { x: vel2.x + acc2.x * dt, y: vel2.y + acc2.y * dt };
-        pos1 = { x: pos1.x + vel1.x * dt, y: pos1.y + vel1.y * dt };
-        pos2 = { x: pos2.x + vel2.x * dt, y: pos2.y + vel2.y * dt };
+function eulerStep(p1, p2, v1, v2, G) {
+    for (var i = 1; i <= 8; i++) {
+        var dt = 1 / i;
+        var acc1 = acceleration(p2, p1, G);
+        var acc2 = acceleration(p1, p2, G);
+        v1 = { x: v1.x + acc1.x * dt, y: v1.y + acc1.y * dt };
+        v2 = { x: v2.x + acc2.x * dt, y: v2.y + acc2.y * dt };
+        p1 = { x: p1.x + v1.x * dt, y: p1.y + v1.y * dt };
+        p2 = { x: p2.x + v2.x * dt, y: p2.y + v2.y * dt };
     }
-    return {pos1: pos1, pos2: pos2, vel1: vel1, vel2: vel2}
+    return [p1, p2, v1, v2];
 }
-
-function acceleration(pos1,pos2,G) {
-    let direction = {x: pos1.x - pos2.x, y: pos1.y - pos2.y} 
-
-    let length = Math.sqrt((direction.x * direction.x) + (direction.y * direction.y));
-
-    let normal = {x: direction.x / length, y: direction.y / length};
-
-    let constant = G/(length*length);
-
-    return {x: normal.x*constant, y: normalY*constant}
+exports.eulerStep = eulerStep;
+function acceleration(p1, p2, G) {
+    var direction = { x: p1.x - p2.x, y: p1.y - p2.y };
+    var length = Math.sqrt((direction.x * direction.x) + (direction.y * direction.y));
+    var normal = { x: direction.x / length, y: direction.y / length };
+    var constant = G / (length * length);
+    return { x: normal.x * constant, y: normal.y * constant };
 }
-
+exports.acceleration = acceleration;
 // #endregion
-
-
 // #region Matrix Operations
-
 function matMul(A, B) {
     if (A[0].length != B.length)
         return null;
@@ -68,126 +62,130 @@ function matMul(A, B) {
     }
     return X;
 }
-
+exports.matMul = matMul;
 // #region rotation 3D
-
 function rotateX3(A, angle) {
     var rotMatrix = [
         [1, 0, 0],
         [0, Math.cos(angle), -Math.sin(angle)],
-        [0, Math.sin(angle), Math.cos(angle)]];
+        [0, Math.sin(angle), Math.cos(angle)]
+    ];
     return matMul(rotMatrix, A);
 }
-
+exports.rotateX3 = rotateX3;
 function rotateY3(A, angle) {
     var rotMatrix = [
         [Math.cos(angle), 0, Math.sin(angle)],
         [0, 1, 0],
-        [-Math.sin(angle), 0, Math.cos(angle)]];
+        [-Math.sin(angle), 0, Math.cos(angle)]
+    ];
     return matMul(rotMatrix, A);
 }
-
+exports.rotateY3 = rotateY3;
 function rotateZ3(A, angle) {
     var rotMatrix = [
         [Math.cos(angle), -Math.sin(angle), 0],
         [Math.sin(angle), Math.cos(angle), 0],
-        [0, 0, 1]];
+        [0, 0, 1]
+    ];
     return matMul(rotMatrix, A);
 }
+exports.rotateZ3 = rotateZ3;
 // #endregion
-
 // #region rotation 4D
-
 function rotateX4(A, angle) {
     var rotMatrix = [
-        [1, 0, 0,0],
-        [0, Math.cos(angle), -Math.sin(angle),0],
-        [0, Math.sin(angle), Math.cos(angle),0],[0,0,0,1]];
+        [1, 0, 0, 0],
+        [0, Math.cos(angle), -Math.sin(angle), 0],
+        [0, Math.sin(angle), Math.cos(angle), 0], [0, 0, 0, 1]
+    ];
     return matMul(rotMatrix, A);
 }
-
+exports.rotateX4 = rotateX4;
 function rotateY4(A, angle) {
     var rotMatrix = [
-        [Math.cos(angle), 0, Math.sin(angle),0],
-        [0, 1, 0,0],
-        [-Math.sin(angle), 0, Math.cos(angle),0],
-        [0,0,0,1]];
+        [Math.cos(angle), 0, Math.sin(angle), 0],
+        [0, 1, 0, 0],
+        [-Math.sin(angle), 0, Math.cos(angle), 0],
+        [0, 0, 0, 1]
+    ];
     return matMul(rotMatrix, A);
 }
-
 function rotateZ4(A, angle) {
     var rotMatrix = [
         [Math.cos(angle), -Math.sin(angle), 0, 0],
         [Math.sin(angle), Math.cos(angle), 0, 0],
-        [0, 0, 1, 0], [0, 0, 0, 1]];
+        [0, 0, 1, 0], [0, 0, 0, 1]
+    ];
     return matMul(rotMatrix, A);
 }
-
+exports.rotateZ4 = rotateZ4;
 function rotateZW4(A, angle) {
     var rotationMatrix = [
         [Math.cos(angle), -Math.sin(angle), 0, 0],
         [Math.sin(angle), Math.cos(angle), 0, 0],
         [0, 0, 1, 0],
         [0, 0, 0, 1],
-    ]
+    ];
     return matMul(rotationMatrix, A);
 }
-
+exports.rotateZW4 = rotateZW4;
 function rotateYW4(A, angle) {
     var rotationMatrix = [
         [Math.cos(angle), 0, -Math.sin(angle), 0],
         [0, 1, 0, 0],
         [Math.sin(angle), 0, Math.cos(angle), 0],
         [0, 0, 0, 1],
-    ]
+    ];
     return matMul(rotationMatrix, A);
 }
-
+exports.rotateYW4 = rotateYW4;
 function rotateYZ4(A, angle) {
     var rotationMatrix = [
         [Math.cos(angle), 0, 0, -Math.sin(angle)],
         [0, 1, 0, 0],
         [0, 0, 1, 0],
         [Math.sin(angle), 0, 0, Math.cos(angle)],
-    ]
+    ];
     return matMul(rotationMatrix, A);
 }
-
+exports.rotateYZ4 = rotateYZ4;
 function rotateXW4(A, angle) {
     var rotationMatrix = [
         [1, 0, 0, 0],
         [0, Math.cos(angle), -Math.sin(angle), 0],
         [0, Math.sin(angle), Math.cos(angle), 0],
         [0, 0, 0, 1],
-    ]
+    ];
     return matMul(rotationMatrix, A);
 }
-
+exports.rotateXW4 = rotateXW4;
 function rotateXZ4(A, angle) {
     var rotationMatrix = [
         [1, 0, 0, 0],
         [0, Math.cos(angle), 0, -Math.sin(angle)],
         [0, 0, 1, 0],
-        [0,  Math.sin(angle), 0, Math.cos(angle)],
-    ]
+        [0, Math.sin(angle), 0, Math.cos(angle)],
+    ];
     return matMul(rotationMatrix, A);
 }
-
+exports.rotateXZ4 = rotateXZ4;
 function rotateXY4(A, angle) {
     var rotationMatrix = [
         [1, 0, 0, 0],
         [0, 1, 0, 0],
         [0, 0, Math.cos(angle), -Math.sin(angle)],
         [0, 0, Math.sin(angle), Math.cos(angle)],
-    ]
+    ];
     return matMul(rotationMatrix, A);
 }
-
+exports.rotateXY4 = rotateXY4;
 // #endregion
-
 // #region Projection
-var project3 = (w) => [[w,0,0,0],[0,w,0,0],[0,0,w,0]];
-var project4 = (z) => [[z,0,0],[0,z,0]];
+var project3 = function (w) { return [[w, 0, 0, 0], [0, w, 0, 0], [0, 0, w, 0]]; };
+exports.project3 = project3;
+var project4 = function (z) { return [[z, 0, 0], [0, z, 0]]; };
+exports.project4 = project4;
 //#endregion
-
 //#endregion
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,195 @@
+// #region Supporting Linear Alg
+
+import { Vector2 } from "./types/vector";
+
+//https://www.desmos.com/calculator/c2wkore2bi
+export function LerpSmooth1D(x1: number, x2: number, p: number, rate: number, halfSmooth: boolean, infinite: boolean): number[] {
+    if(infinite || p <= Math.PI * (halfSmooth ? .5 : 1)) {
+        p += rate;
+        return [((Math.sin(p + (1.5 * Math.PI))+1)*((x2-x1)/(halfSmooth ? 1 : 2)) + x1), p];
+    }
+    return [x2, p]
+}
+
+//fraction > 0; fraction = N
+//0 <= fpos < fraction; fpos = N
+//https://www.desmos.com/calculator/tbxtssvtcn
+export function fractionalLine(p1: Vector2, p2: Vector2, f: number, p: number): number[] {
+    if(f <= 0 || p < 0 || p >= f)
+        return [p1.x,p1.y,p2.x,p2.y];
+    let dx = (p2.x - p1.x)/f;
+    let dy = (p2.y - p1.y)/f;
+    return [dx*p+p1.x,dy*p+p1.y,dx*(p+1)+p1.x,dy*(p+1)+p1.y];
+}
+
+// #region Oribital Mechanics
+
+//https://www.desmos.com/calculator/vbsetpue7m
+export function eulerStep(p1: Vector2, p2: Vector2, v1: Vector2, v2: Vector2, G: number): Vector2[] {
+    for(let i = 1; i <= 8; i++) {
+        let dt = 1 / i;
+        let acc1: Vector2 = acceleration(p2, p1, G);
+        let acc2: Vector2 = acceleration(p1, p2, G);
+        v1 = { x: v1.x + acc1.x * dt, y: v1.y + acc1.y * dt };
+        v2 = { x: v2.x + acc2.x * dt, y: v2.y + acc2.y * dt };
+        p1 = { x: p1.x + v1.x * dt, y: p1.y + v1.y * dt };
+        p2 = { x: p2.x + v2.x * dt, y: p2.y + v2.y * dt };
+    }
+    return [p1, p2, v1, v2];
+}
+
+export function acceleration(p1: Vector2, p2: Vector2, G: number): Vector2 {
+    let direction: Vector2 = {x: p1.x - p2.x, y: p1.y - p2.y} 
+
+    let length = Math.sqrt((direction.x * direction.x) + (direction.y * direction.y));
+
+    let normal = {x: direction.x / length, y: direction.y / length};
+
+    let constant = G/(length*length);
+
+    return {x: normal.x*constant, y: normal.y*constant}
+}
+
+// #endregion
+
+
+// #region Matrix Operations
+
+export function matMul(A: number[][], B: number[][]): number[][] | null {
+    if (A[0].length != B.length)
+        return null;
+    var X = new Array(A.length);
+    for (var i = 0; i < A.length; i++) {
+        X[i] = new Array(B[0].length);
+        for (var j = 0; j < B[0].length; j++) {
+            var sum = 0;
+            for (var k = 0; k < A[0].length; k++)
+                sum += A[i][k] * B[k][j];
+            X[i][j] = sum;
+        }
+    }
+    return X;
+}
+
+// #region rotation 3D
+
+export function rotateX3(A: number[][], angle: number): number[][] | null {
+    let rotMatrix = [
+        [1, 0, 0],
+        [0, Math.cos(angle), -Math.sin(angle)],
+        [0, Math.sin(angle), Math.cos(angle)]];
+    return matMul(rotMatrix, A);
+}
+
+export function rotateY3(A: number[][], angle: number): number[][] | null {
+    let rotMatrix = [
+        [Math.cos(angle), 0, Math.sin(angle)],
+        [0, 1, 0],
+        [-Math.sin(angle), 0, Math.cos(angle)]];
+    return matMul(rotMatrix, A);
+}
+
+export function rotateZ3(A: number[][], angle: number): number[][] | null {
+    let rotMatrix = [
+        [Math.cos(angle), -Math.sin(angle), 0],
+        [Math.sin(angle), Math.cos(angle), 0],
+        [0, 0, 1]];
+    return matMul(rotMatrix, A);
+}
+// #endregion
+
+// #region rotation 4D
+
+export function rotateX4(A: number[][], angle: number): number[][] | null {
+    let rotMatrix = [
+        [1, 0, 0,0],
+        [0, Math.cos(angle), -Math.sin(angle),0],
+        [0, Math.sin(angle), Math.cos(angle),0],[0,0,0,1]];
+    return matMul(rotMatrix, A);
+}
+
+function rotateY4(A: number[][], angle: number): number[][] | null {
+    let rotMatrix = [
+        [Math.cos(angle), 0, Math.sin(angle),0],
+        [0, 1, 0,0],
+        [-Math.sin(angle), 0, Math.cos(angle),0],
+        [0,0,0,1]];
+    return matMul(rotMatrix, A);
+}
+
+export function rotateZ4(A: number[][], angle: number): number[][] | null {
+    let rotMatrix = [
+        [Math.cos(angle), -Math.sin(angle), 0, 0],
+        [Math.sin(angle), Math.cos(angle), 0, 0],
+        [0, 0, 1, 0], [0, 0, 0, 1]];
+    return matMul(rotMatrix, A);
+}
+
+export function rotateZW4(A: number[][], angle: number): number[][] | null {
+    let rotationMatrix = [
+        [Math.cos(angle), -Math.sin(angle), 0, 0],
+        [Math.sin(angle), Math.cos(angle), 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1],
+    ]
+    return matMul(rotationMatrix, A);
+}
+
+export function rotateYW4(A: number[][], angle: number): number[][] | null {
+    let rotationMatrix = [
+        [Math.cos(angle), 0, -Math.sin(angle), 0],
+        [0, 1, 0, 0],
+        [Math.sin(angle), 0, Math.cos(angle), 0],
+        [0, 0, 0, 1],
+    ]
+    return matMul(rotationMatrix, A);
+}
+
+export function rotateYZ4(A: number[][], angle:number): number[][] | null {
+    let rotationMatrix = [
+        [Math.cos(angle), 0, 0, -Math.sin(angle)],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [Math.sin(angle), 0, 0, Math.cos(angle)],
+    ]
+    return matMul(rotationMatrix, A);
+}
+
+export function rotateXW4(A:number[][], angle: number): number[][] | null {
+    let rotationMatrix = [
+        [1, 0, 0, 0],
+        [0, Math.cos(angle), -Math.sin(angle), 0],
+        [0, Math.sin(angle), Math.cos(angle), 0],
+        [0, 0, 0, 1],
+    ]
+    return matMul(rotationMatrix, A);
+}
+
+export function rotateXZ4(A: number[][], angle: number): number[][] | null {
+    let rotationMatrix = [
+        [1, 0, 0, 0],
+        [0, Math.cos(angle), 0, -Math.sin(angle)],
+        [0, 0, 1, 0],
+        [0,  Math.sin(angle), 0, Math.cos(angle)],
+    ]
+    return matMul(rotationMatrix, A);
+}
+
+export function rotateXY4(A: number[][], angle: number): number[][] | null {
+    let rotationMatrix = [
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, Math.cos(angle), -Math.sin(angle)],
+        [0, 0, Math.sin(angle), Math.cos(angle)],
+    ]
+    return matMul(rotationMatrix, A);
+}
+
+// #endregion
+
+// #region Projection
+export const project3 = (w: number): number[][] => [[w,0,0,0],[0,w,0,0],[0,0,w,0]];
+export const project4 = (z: number): number[][] => [[z,0,0],[0,z,0]];
+//#endregion
+
+//#endregion
